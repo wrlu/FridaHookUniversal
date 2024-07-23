205aab9fa1f0805f99b37f1bec6213028843a28f
wrlu <wrlu.cn@outlook.com>
2024-07-23 15:53:45 +0800

    Update

--- FridaHook.py
+++ FridaHook.py
@@ -3,16 +3,16 @@ import frida
 
 # Full file name: hook_[platform]_[name].js
 js_modules = [
-    {'platform': 'android', 'name': 'webview'},
+    {'platform': 'ios', 'name': 'scheme'},
 ]
 
 # For frida version 15 or higher
 # Please use real App name for `name` and package name for `identifier` on Android
 # For Android native processes or iOS processes, keep identifier and name as same
 processes_to_hook = [
-    {'identifier': 'com.ss.android.ugc.aweme', 'name': '抖音'}, # Android
+    # {'identifier': 'com.ss.android.ugc.aweme', 'name': '抖音'}, # Android
     # {'identifier': 'com.ss.iphone.ugc.Aweme', 'name': '抖音'}, # iOS
-    # {'identifier': 'com.ss.iphone.ugc.Aweme', 'name': '抖音内测'}, # iOS
+    # {'identifier': '95526.mobi', 'name': '北京银行'}, # iOS
 ]
 
 class Log:
@@ -40,10 +40,13 @@ class Log:
 def on_message(message, data):
     if message['type'] == 'send':
         Log.send(message['payload'])
+        with open('save.log', 'a+') as f:
+            f.write(message['payload'])
+            f.write('\n')
     elif message['type'] == 'error':
-        Log.error(message['description'])
+        Log.error(message['stack'])
     else:
-        Log.error(message)
+        Log.error(str(message))
 
 
 def init_device():
@@ -76,7 +79,6 @@ def init_device():
 if __name__ == '__main__':
     try:
         device = init_device()
-        all_processes = device.enumerate_processes()
         for per_hook_process in processes_to_hook:
             try:
                 device.get_process(per_hook_process['name'])
